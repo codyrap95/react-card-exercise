ae2666cf56a96d97ade5ea94ea4841c6d9b6232b
Cody Dubat <codydjay@gmail.com>
2021-10-16 17:11:13 +0300

    Main styling done

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import "./App.css";
 function App() {
   return (
     <main className="card">
-      <img src={headerImage} alt="" />
-      <h1>Order Summary</h1>
-      <p>
-        You can now listen to millions of songs, audiobooks and podcasts on any
-        device anywhere you like.
-      </p>
-      <div className="subcard">
-        <img src={musicNote} alt="" />
-        <span className="subcard__middle">
-          <h2>Annual Plan</h2>
-          <p>$59.99/year</p>
-        </span>
-        <a href="/">Change</a>
-      </div>
-      <button className="btn btn--highlighted">Proceed to payment</button>
-      <button className="btn">Cancel Order</button>
+      <img className="hero-img" src={headerImage} alt="" />
+      <section className="content">
+        <h1>Order Summary</h1>
+        <p>
+          You can now listen to millions of songs, audiobooks and podcasts on
+          any device anywhere you like.
+        </p>
+        <div className="subcard">
+          <img src={musicNote} alt="" />
+          <span className="subcard__middle">
+            <h2>Annual Plan</h2>
+            <p>$59.99/year</p>
+          </span>
+          <a href="/">Change</a>
+        </div>
+        <button className="btn btn--highlighted">Proceed to payment</button>
+        <button className="btn">Cancel Order</button>
+      </section>
     </main>
   );
 }
