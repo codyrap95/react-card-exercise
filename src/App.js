f78fef376d8fddb14e17b016a4f836ca8cf199c1
Cody Dubat <codydjay@gmail.com>
2021-10-16 15:50:38 +0300

    corrections

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import "./App.css";
 function App() {
   return (
     <main className="card">
-      <img src={headerImage} alt="" srcset="" />
+      <img src={headerImage} alt="" />
       <h1>Order Summary</h1>
       <p>
         You can now listen to millions of songs, audiobooks and podcasts on any
         device anywhere you like.
       </p>
       <div className="subcard">
-        <img src="" alt="" srcset="" />
+        <img src="" alt="" />
         <div className="subcard__middle"></div>
-        <a href="#">Change</a>
+        <a href="/">Change</a>
       </div>
       <button className="btn btn--highlighted">Proceed to payment</button>
       <button className="btn">Cancel Order</button>
